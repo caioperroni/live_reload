c917ca03743ec06698a3f3107bcba1ce8e9db533
Caio <caio@perroni.tech>
2022-11-28 19:54:14 -0300

    new strategy multi app

--- main.js
+++ main.js
@@ -1,23 +1,31 @@
-// live_reload ♻ node - spawn to init child process and watch for file changes
+// live_reload ♻ node libs
 const { spawn } = require("child_process");
 const { watch, writeFileSync } = require("fs");
 
-// live_reload ♻ constants - from main.json
-const { config, paths, msgs } = require("./main.json");
-const { ioPath, mainPath, signalPath } = paths;
-const { listening } = msgs;
-const { appName, debugOnConsole, port } = config;
-
 // live_reload ♻ helpers - from local/helper
-const { logColor, liveReloadError, liveReloadSocket } = require("./local/helper/helper.js");
+const { liveReloadError, liveReloadWorkerStart } = require("./local/helper/helper.js");
+
+// live_reload ♻ constants - from main.json
+const { watchConfig, watchOptions, watchWorker } = require("./main.json");
+const { mainCmd, entryPath, preArgs, postArgs, watchDir } = watchConfig;
+const { debugOnConsole, signalPath } = watchOptions;
+const { workerWatch, workerOrigin } = watchWorker;
 
 // live_reload ♻ variables - to handle child proccess and args/parameters
 let liveReloadChild;
 let liveReloadArg = process.argv[2];
 
+// live_reload ♻ kill - assure liveReloadChild and group is killed
+const liveReloadKill = () => {
+  try {
+    process.kill(-liveReloadChild.pid);
+  } catch {}
+};
+
 // live_reload ♻ bind - bind to child process "close" event to respawn/reload
 // and stderr data event to log errors if debugOnConsole is disabled on main.json
 const liveReloadBind = (child) => {
+  child.on("exit", liveReloadKill);
   child.on("close", (code) => {
     if (code === 0) liveReloadSpawn(liveReloadArg);
     else liveReloadFix();
@@ -27,9 +35,14 @@ const liveReloadBind = (child) => {
 
 // live_reload ♻ spawn - creates child process and call event binder
 const liveReloadSpawn = (arg) => {
+  liveReloadBashWatch();
   writeFileSync(signalPath + ".node", "spawn");
-  liveReloadChild = spawn("node", ["main.js", "child", arg], {
-    stdio: [null, 1, debugOnConsole ? 2 : null],
+  const entry = `${watchDir}/${entryPath}`;
+  const args = [].concat(preArgs, entry, postArgs);
+  liveReloadChild = spawn(mainCmd, args, {
+    // stdio: [null, 1, debugOnConsole ? 2 : null],
+    stdio: ["pipe", 1, debugOnConsole ? 2 : "pipe"],
+    detached: true,
   });
   liveReloadBind(liveReloadChild);
 };
@@ -46,10 +59,10 @@ const liveReloadNodeWatch = () => liveReloadWatch(signalPath + ".node", () => li
 // live_reload ♻ bash watch - watch for new changes, emiting io event and exiting from child proccess
 const liveReloadBashWatch = () => {
   liveReloadWatch(signalPath + ".bash", () => {
-    if (_io) _io.emit("reload");
-    process.exit(0);
+    if (workerWatch) _io.emit("reload");
+    liveReloadChild.exitCode = 0;
+    liveReloadChild.kill();
   });
-  liveReloadStart();
 };
 
 // live_reload ♻ watch -  watch for new changes wrapper
@@ -60,16 +73,8 @@ const liveReloadWatch = (path, callback) => {
   });
 };
 
-// live_reload ♻ start - require main app, handle io and start
-const liveReloadStart = () => {
-  const app = require(mainPath);
-  app.get(ioPath, (req, res) => res.sendStatus(200));
-  const http = require("http").Server(app);
-  _io = require("socket.io")(http);
-  _io.on("connection", liveReloadSocket);
-  http.listen(port, () => logColor("32", `${appName} ${listening} ${port}`));
-};
-
 // live_reload ♻ entry - handle first liveReload call to spwan or watch child process
-if (liveReloadArg === "child") liveReloadBashWatch();
-else liveReloadSpawn(liveReloadArg);
+liveReloadSpawn(liveReloadArg);
+if (workerWatch) liveReloadWorkerStart(workerOrigin);
+process.on("exit", liveReloadKill);
+process.on("SIGINT", () => process.exit());
